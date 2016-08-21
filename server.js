ec5dfa66143198ee1e6e8bec43326ee207cb0d05
Sylver Yagi <silvery4543@gmail.com>
2016-08-21 23:27:21 +1000

    ver. 2.1.2

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var cheerio = require("cheerio");
 var cookieSession = require("cookie-session");
 var weather = require("./lib/weather-config.js");
+var unxConv = require("./lib/unixdate-converter.js");
 
 app.use(bodyParser.json());
 app.use(cookieSession({
 	name: "sampleSession",
 	secret: "monobelle",
 	img: "http://www.google.com"
-}));
+})); 
 
 app.post("/", function (req, res) {
 	res.redirect("/");
 });
-app.get('/', function(req,res) {
+app.get("/", function(req,res) {
 	weather.getCurrent("Lae", function(err, data) {
-		res.render("pages/weather",{weda:data});
+		data.dt = unxConv(data.dt);
+		data.sys.sunrise = unxConv(data.sys.sunrise);
+		data.sys.sunset = unxConv(data.sys.sunset);
+		err ? res.send(err) : res.render("pages/weather",{weda:data});
 	});
 });
 
 app.get('/api/', function (req, res) {
 	console.log(req.query.city);
 	weather.getCurrent(req.query.city, function(err, data) {
+		data.dt = unxConv(data.dt);
+		data.sys.sunrise = unxConv(data.sys.sunrise);
+		data.sys.sunset = unxConv(data.sys.sunset);
 		err ? res.send(err) : res.render("pages/weather",{weda:data});
 	});
 });
