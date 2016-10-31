aaef9f6f68c2ab061e777595e089de58d5d9e2ea
Sylver Yagi <silvery4543@gmail.com>
2016-10-31 15:07:25 +1000

    ver2.1.7 - fb login

--- public/scripts/facebook-integration.js
+++ public/scripts/facebook-integration.js
@@ -33,11 +33,10 @@ window.fbAsyncInit = function() {
   });
 
 };
-
-(function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1476643879330794";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1476643879330794";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
