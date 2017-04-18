8beb2485a93f5315bb3fc0cbddc1673f6dbea694
Sylver Yagi <silvery4543@gmail.com>
2017-04-18 16:32:12 +1000

    2.2.3

--- public/scripts/facebook-integration.js
+++ public/scripts/facebook-integration.js
@@ -15,7 +15,7 @@ window.fbAsyncInit = function() {
         var propicBlock = document.getElementById('fb-propic');
         welcomeBlock.innerHTML = 'Greetings ' + response.first_name + '!';
         locationBlock.innerHTML = 'You are in ' + response.location.name;
-        propicBlock.innerHTML = '<img class="img-responsive" src="'+response.picture.url+'" />';
+        propicBlock.innerHTML = '<img class="img-responsive" src="'+response.picture.data.url+'" />';
       });
     }
   }
